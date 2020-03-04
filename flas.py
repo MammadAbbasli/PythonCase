23ec296d6721d6c8e5c65603caa9c2a19bf136cb
MammadAbbasli <57585683+MammadAbbasli@users.noreply.github.com>
2020-03-05 00:15:46 +0300

    Update flas.py

--- flas.py
+++ flas.py
@@ -1,15 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  3 10:54:55 2020
 
-@author: mamed
-"""
-
-from flask import Flask,render_template
-
-app=Flask(__name__)
-@app.route("/")
-def index():
-    return "Ana sayfa"
-if __name__=="__main__":
-    app.run()
