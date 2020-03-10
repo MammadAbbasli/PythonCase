f8adea498f69117cd40164cc0f36961f69aa836c
MammadAbbasli <57585683+MammadAbbasli@users.noreply.github.com>
2020-03-10 20:47:33 +0300

    flash.py

--- flash.py
+++ flash.py
@@ -2,13 +2,11 @@
 """
 Created on Tue Mar  3 12:54:33 2020
 
-@author: mamed
+@author: Mammad Abbasli
 """
 
 from flask import Flask,jsonify
 from flask_mysqldb import MySQL
-
-
 
 app=Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
@@ -18,20 +16,19 @@ app.config["MYSQL_DB"] = "case"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
-@app.route("/<extColor>/<model>/<year>",methods=['GET'])
 
-def liste(extColor,model,year):
-    print(extColor,model,year)
+@app.route("/list/<string:color>/<string:model>/<string:year>",methods=['GET'])
+
+def liste(color,model,year):
     cursor = mysql.connection.cursor()
-    sorgu = ("Select * From car_info where ext_color='{0}'and models='{1}'and year='{2}'".format(extColor,model,year))
+    sorgu = "Select * From car_info where ext_color= 'color' and models='model' and year='year'"
     cursor.execute(sorgu)
     mysql.connection.commit()
     data=cursor.fetchall()
+    print(data)
     cursor.close()
     return jsonify(data)
 
 if __name__=="__main__":
-#    app.debug = True
-    use_reloader=False
-    app.run()
 
+    app.run()
